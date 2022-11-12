16ac23b3c3cddfe63f142801db10504f8e2a5c85
Ross Gordon <rossagordonstl@gmail.com>
2022-11-12 12:08:27 -0500

    Landing page section complete

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,15 +12,11 @@ const Projects = () => {
         <div className="pb-8">
           <p className="text-5xl font-bold inline">Projects</p>
           <p className="py-6 text-xl">Here are a few of my recent projects:</p>
-          <p className="text-2xl">
-            I am always working on new projects. Currently, I'm working on my
-            React skills.
-          </p>
         </div>
 
         {/* Projects */}
         <div className="grid sm:grid-cols-2  md:grid-cols-3 gap-4 mt-[-20px] text-black">
-          {/* Project 1 • Good Coffee */}
+          {/* Project 1 • Title */}
           <div className="rounded-md shadow-lg bg-[#fff]">
             <a href="https://coffeeshopsite.netlify.app" target="_blank">
               <img
@@ -122,39 +118,34 @@ const Projects = () => {
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full">
         <div className="pb-8">
           <p className="text-5xl font-bold inline">Landing Pages</p>
-          <p className="py-6 text-xl">Here are a few of my recent projects:</p>
-          <p className="text-2xl">
-            I am always working on new projects. Currently, I'm working on my
-            React skills.
-          </p>
+          <p className="py-6 text-xl">Here are a few landing pages I've made:</p>
         </div>
 
         {/* Projects */}
         <div className="grid sm:grid-cols-2  md:grid-cols-3 gap-4 mt-[-20px] text-black">
-          {/* Project 1 • Good Coffee */}
+          {/* Project 1 • Edgewood Contracting */}
           <div className="rounded-md shadow-lg bg-[#fff]">
-            <a href="https://coffeeshopsite.netlify.app" target="_blank">
+            <a href="https://www.edgewoodcontracting.com" target="_blank">
               <img
-                src={Pourover}
+                src={Edgewood}
                 className="rounded-lg hover:scale-105 duration-300"
                 alt="A hand poured latte next to a laptop"
               />
             </a>
             <div className="px-6 py-4 text-center">
-              <div className="font-bold text-xl mb-2 text-center">Title</div>
+              <div className="font-bold text-xl mb-2 text-center">Edgewood Contracting</div>
               <p className="pb-2">Description</p>
               <p className="font-extrabold">Technologies Used:</p>
-              <p>Lorem ipsum dolor sit.</p>
+              <p>React • Tailwind CSS</p>
             </div>
             <div className="text-center mt-5">
-              <a href="https://coffeeshopsite.netlify.app/" target="_blank">
+              <a href="https://www.edgewoodcontracting.com" target="_blank">
                 <button className="text-center rounded-lg px-2 py-2 m-2 bg-[#fff] text-[#000] border font-bold text-lg hover:bg-[#000] hover:text-[#fff]">
                   Demo
                 </button>
               </a>
               <a
-                href="https://github.com/rossagordon/Coffee-Shop-Landing-Page"
-                target="_blank"
+                href="https://github.com/rossagordon/Edgewood-Contracting" target="_blank"
               >
                 <button className="text-center rounded-lg px-2 py-2 m-2 bg-[#fff] text-[#000] border font-bold text-lg hover:bg-[#000] hover:text-[#fff]">
                   Code
@@ -163,30 +154,29 @@ const Projects = () => {
             </div>
           </div>
 
-          {/* Project 2 • Title */}
+          {/* Project 2 • Lotus of my Heart */}
           <div className="rounded-md shadow-lg bg-[#fff]">
-            <a href="https://coffeeshopsite.netlify.app" target="_blank">
+            <a href="https://www.lotusofmyheart.net/" target="_blank">
               <img
-                src={Pourover}
+                src={Scorpion}
                 className="rounded-lg hover:scale-105 duration-300"
                 alt="A hand poured latte next to a laptop"
               />
             </a>
             <div className="px-6 py-4 text-center">
-              <div className="font-bold text-xl mb-2 text-center">Title</div>
+              <div className="font-bold text-xl mb-2 text-center">Lotus of my Heart</div>
               <p className="pb-2">Description</p>
               <p className="font-extrabold">Technologies Used:</p>
-              <p>Lorem ipsum dolor sit.</p>
+              <p>React • Tailwind CSS</p>
             </div>
             <div className="text-center mt-5">
-              <a href="https://coffeeshopsite.netlify.app/" target="_blank">
+              <a href="https://www.lotusofmyheart.net/" target="_blank">
                 <button className="text-center rounded-lg px-2 py-2 m-2 bg-[#fff] text-[#000] border font-bold text-lg hover:bg-[#000] hover:text-[#fff]">
                   Demo
                 </button>
               </a>
               <a
-                href="https://github.com/rossagordon/Coffee-Shop-Landing-Page"
-                target="_blank"
+                href="https://github.com/rossagordon/Lotus-of-My-Heart" target="_blank"
               >
                 <button className="text-center rounded-lg px-2 py-2 m-2 bg-[#fff] text-[#000] border font-bold text-lg hover:bg-[#000] hover:text-[#fff]">
                   Code
@@ -195,7 +185,7 @@ const Projects = () => {
             </div>
           </div>
 
-          {/* Project 3 • Title */}
+          {/* Project 3 • Good Coffee */}
           <div className="rounded-md shadow-lg bg-[#fff]">
             <a href="https://coffeeshopsite.netlify.app" target="_blank">
               <img
@@ -205,10 +195,10 @@ const Projects = () => {
               />
             </a>
             <div className="px-6 py-4 text-center">
-              <div className="font-bold text-xl mb-2 text-center">Title</div>
+              <div className="font-bold text-xl mb-2 text-center">Good Coffee</div>
               <p className="pb-2">Description</p>
               <p className="font-extrabold">Technologies Used:</p>
-              <p>Lorem ipsum dolor sit.</p>
+              <p>React • Tailwind CSS</p>
             </div>
             <div className="text-center mt-5">
               <a href="https://coffeeshopsite.netlify.app/" target="_blank">
